724adf23f0c4205e1d307fd57452f470fa790179
helenajr <helenajrobinson@gmail.com>
2025-05-22 09:15:36 +0100

    removed unnecessary folder

--- app/launch.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-pg = st.navigation([
-        st.Page("home_page.py", title="Welcome!", icon=":material/add_circle:"),
-        st.Page("sim_page.py", title="Run Simulation", icon=":material/laptop:"),
-        st.Page("more_info_page.py", title="More Information", icon=":material/book:")
-     ])
-
-pg.run()
